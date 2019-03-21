ddc8623374ee8e10bb4f388d564c4a757bf6c0c0
Witawe <texys@list.ru>
2019-03-21 09:33:53 +0900

    lox

--- GitHub_2/GitHub_2/AAA_project_test.cpp
+++ GitHub_2/GitHub_2/AAA_project_test.cpp
@@ -2,7 +2,6 @@
 
 #define BOOST_TEST_MODULE TEST1
 #include <boost/test/included/unit_test.hpp>
-
 #include "test_Header.h"
 
 BOOST_AUTO_TEST_CASE( test_case1 ){
@@ -28,7 +27,28 @@ BOOST_AUTO_TEST_CASE( test_case1 ){
 	BOOST_TEST_CHECK(numbers_lt_20_to_string(19)==string("Девятьнадцать"));	
 }
 BOOST_AUTO_TEST_CASE( test_case2 ){
-	BOOST_TEST_CHECK(numbers_lt_100_to_string(20)==string("Двадцать"));
+	BOOST_TEST_CHECK(numbers_lt_100_to_string(0)==string("Ноль"));
+    BOOST_TEST_CHECK(numbers_lt_100_to_string(1)==string("Один"));
+	BOOST_TEST_CHECK(numbers_lt_100_to_string(2)==string("Два"));
+	BOOST_TEST_CHECK(numbers_lt_100_to_string(3)==string("Три"));
+	BOOST_TEST_CHECK(numbers_lt_100_to_string(4)==string("Четыре"));
+	BOOST_TEST_CHECK(numbers_lt_100_to_string(5)==string("Пять"));
+	BOOST_TEST_CHECK(numbers_lt_100_to_string(6)==string("Шесть"));
+	BOOST_TEST_CHECK(numbers_lt_100_to_string(7)==string("Семь"));
+	BOOST_TEST_CHECK(numbers_lt_100_to_string(8)==string("Восемь"));
+	BOOST_TEST_CHECK(numbers_lt_100_to_string(9)==string("Девять"));
+	BOOST_TEST_CHECK(numbers_lt_100_to_string(10)==string("Десять"));
+	BOOST_TEST_CHECK(numbers_lt_100_to_string(11)==string("Одиннадцать"));
+	BOOST_TEST_CHECK(numbers_lt_100_to_string(12)==string("Двенадцать"));
+	BOOST_TEST_CHECK(numbers_lt_100_to_string(13)==string("Тринадцать"));
+	BOOST_TEST_CHECK(numbers_lt_100_to_string(14)==string("Четырнадцать"));
+	BOOST_TEST_CHECK(numbers_lt_100_to_string(15)==string("Пятьнадцать"));
+	BOOST_TEST_CHECK(numbers_lt_100_to_string(16)==string("Шестьнадцать"));
+	BOOST_TEST_CHECK(numbers_lt_100_to_string(17)==string("Семьнадцать"));
+	BOOST_TEST_CHECK(numbers_lt_100_to_string(18)==string("Восемьнадцать"));
+	BOOST_TEST_CHECK(numbers_lt_100_to_string(19)==string("Девятьнадцать"));	
+	BOOST_TEST_CHECK(numbers_lt_100_to_string(7)==string("Семь"));
+	BOOST_TEST_CHECK(numbers_lt_100_to_string(10)==string("Десять"));
 	BOOST_TEST_CHECK(numbers_lt_100_to_string(21)==string("Двадцать Один"));
 	BOOST_TEST_CHECK(numbers_lt_100_to_string(32)==string("Тридцать Два"));
 	BOOST_TEST_CHECK(numbers_lt_100_to_string(43)==string("Сорок Три"));
@@ -39,6 +59,16 @@ BOOST_AUTO_TEST_CASE( test_case2 ){
 	BOOST_TEST_CHECK(numbers_lt_100_to_string(97)==string("Девяноста Семь"));
 }
 BOOST_AUTO_TEST_CASE( test_case3 ){
+	BOOST_TEST_CHECK(numbers_lt_1000_to_string(7)==string("Семь"));
+	BOOST_TEST_CHECK(numbers_lt_1000_to_string(10)==string("Десять"));
+	BOOST_TEST_CHECK(numbers_lt_1000_to_string(21)==string("Двадцать Один"));
+	BOOST_TEST_CHECK(numbers_lt_1000_to_string(32)==string("Тридцать Два"));
+	BOOST_TEST_CHECK(numbers_lt_1000_to_string(43)==string("Сорок Три"));
+	BOOST_TEST_CHECK(numbers_lt_1000_to_string(54)==string("Пятьдесять Четыре"));
+	BOOST_TEST_CHECK(numbers_lt_1000_to_string(65)==string("Шестьдесять Пять"));
+	BOOST_TEST_CHECK(numbers_lt_1000_to_string(79)==string("Семьдесять Девять"));
+	BOOST_TEST_CHECK(numbers_lt_1000_to_string(86)==string("Восемьдесять Шесть"));
+	BOOST_TEST_CHECK(numbers_lt_1000_to_string(97)==string("Девяноста Семь"));
 	BOOST_TEST_CHECK(numbers_lt_1000_to_string(100)==string("Сто"));
 	BOOST_TEST_CHECK(numbers_lt_1000_to_string(120)==string("Сто Двадцать"));
 	BOOST_TEST_CHECK(numbers_lt_1000_to_string(123)==string("Сто Двадцать Три"));
@@ -50,5 +80,21 @@ BOOST_AUTO_TEST_CASE( test_case3 ){
 	BOOST_TEST_CHECK(numbers_lt_1000_to_string(700)==string("Семьсот"));
 	BOOST_TEST_CHECK(numbers_lt_1000_to_string(800)==string("Восемьсот"));
 	BOOST_TEST_CHECK(numbers_lt_1000_to_string(900)==string("Девятсот"));
+}
+BOOST_AUTO_TEST_CASE( test_case4 ){
+	BOOST_TEST_CHECK(currency_to_string(25, "рубль", "рубля", "рублей", 73, "копейка", "копейки", "копеек"));
+	BOOST_TEST_CHECK(currency_to_string(85, "рубль", "рубля", "рублей", 75, "копейка", "копейки", "копеек"));
+	BOOST_TEST_CHECK(currency_to_string(15, "рубль", "рубля", "рублей", 79, "копейка", "копейки", "копеек"));
+	BOOST_TEST_CHECK(currency_to_string(46, "рубль", "рубля", "рублей", 78, "копейка", "копейки", "копеек"));
+	BOOST_TEST_CHECK(currency_to_string(25, "рубль", "рубля", "рублей", 78, "копейка", "копейки", "копеек"));
+	BOOST_TEST_CHECK(currency_to_string(11, "рубль", "рубля", "рублей", 43, "копейка", "копейки", "копеек"));
+	BOOST_TEST_CHECK(currency_to_string(22, "рубль", "рубля", "рублей", 53, "копейка", "копейки", "копеек"));
+	BOOST_TEST_CHECK(currency_to_string(21, "рубль", "рубля", "рублей", 13, "копейка", "копейки", "копеек"));
+	BOOST_TEST_CHECK(currency_to_string(30, "рубль", "рубля", "рублей", 53, "копейка", "копейки", "копеек"));
+	BOOST_TEST_CHECK(currency_to_string(15, "рубль", "рубля", "рублей", 23, "копейка", "копейки", "копеек"));
+	BOOST_TEST_CHECK(currency_to_string(49, "рубль", "рубля", "рублей", 13, "копейка", "копейки", "копеек"));
+	BOOST_TEST_CHECK(currency_to_string(22, "рубль", "рубля", "рублей", 43, "копейка", "копейки", "копеек"));
+	BOOST_TEST_CHECK(currency_to_string(52, "рубль", "рубля", "рублей", 73, "копейка", "копейки", "копеек"));
+	BOOST_TEST_CHECK(currency_to_string(43, "рубль", "рубля", "рублей", 2, "копейка", "копейки", "копеек"));
 }
 #endif
--- GitHub_2/GitHub_2/test_Header.h
+++ GitHub_2/GitHub_2/test_Header.h
@@ -4,75 +4,104 @@ using namespace std;
 
 string numbers_lt_20_to_string(int number){
 	string answer;
-	switch(number){
-	case 0:answer = "Ноль";break;
-	case 1:answer = "Один";break;
-	case 2:answer = "Два";break;
-	case 3:answer = "Три";break;
-	case 4:answer = "Четыре";break;
-	case 5:answer = "Пять";break;
-	case 6:answer = "Шесть";break;
-	case 7:answer = "Семь";break;
-	case 8:answer = "Восемь";break;
-	case 9:answer = "Девять";break;
-	case 10:answer = "Десять";break;
-	case 11:answer = "Одиннадцать";break;
-	case 12:answer = "Двенадцать";break;
-	case 13:answer = "Тринадцать";break;
-	case 14:answer = "Четырнадцать";break;
-	case 15:answer = "Пятьнадцать";break;
-	case 16:answer = "Шестьнадцать";break;
-	case 17:answer = "Семьнадцать";break;
-	case 18:answer = "Восемьнадцать";break;
-	case 19:answer = "Девятьнадцать";break;
-	default: break;
-	}
+		switch(number){
+			case 0:answer = "Ноль";break;
+			case 1:answer = "Один";break;
+			case 2:answer = "Два";break;
+			case 3:answer = "Три";break;
+			case 4:answer = "Четыре";break;
+			case 5:answer = "Пять";break;
+			case 6:answer = "Шесть";break;
+			case 7:answer = "Семь";break;
+			case 8:answer = "Восемь";break;
+			case 9:answer = "Девять";break;
+			case 10:answer = "Десять";break;
+			case 11:answer = "Одиннадцать";break;
+			case 12:answer = "Двенадцать";break;
+			case 13:answer = "Тринадцать";break;
+			case 14:answer = "Четырнадцать";break;
+			case 15:answer = "Пятьнадцать";break;
+			case 16:answer = "Шестьнадцать";break;
+			case 17:answer = "Семьнадцать";break;
+			case 18:answer = "Восемьнадцать";break;
+			case 19:answer = "Девятьнадцать";break;
+			default: break;
+		}
 	return answer;	
 }
+
 string numbers_lt_100_to_string(int number){
 	string answer;
 	int tens = number / 10;
-	int remainder = number % 10;
-	/*if (number < 20) answer = numbers_lt_20_to_string(number);*/
-	switch(tens){
-	case 2:answer = "Двадцать";break;
-	case 3:answer = "Тридцать";break;
-	case 4:answer = "Сорок";break;
-	case 5:answer = "Пятьдесять";break;
-	case 6:answer = "Шестьдесять";break;
-	case 7:answer = "Семьдесять";break;
-	case 8:answer = "Восемьдесять";break;
-	case 9:answer = "Девяноста";break;
-	default:break;
-	}
-	if (remainder != 0)
-	{
+	int remainder = number % 10;	
+		switch(tens){
+			case 1:answer = "Десять";break;
+			case 2:answer = "Двадцать";break;
+			case 3:answer = "Тридцать";break;
+			case 4:answer = "Сорок";break;
+			case 5:answer = "Пятьдесять";break;
+			case 6:answer = "Шестьдесять";break;
+			case 7:answer = "Семьдесять";break;
+			case 8:answer = "Восемьдесять";break;
+			case 9:answer = "Девяноста";break;
+			default:break;
+		}
+	if (number < 20) 
+		answer = numbers_lt_20_to_string(number);
+	else if (remainder != 0)
 		answer +=" "+numbers_lt_20_to_string(remainder);
-	}
 	return answer;
 }
+
 string numbers_lt_1000_to_string(int number){
-	string answer;
+	string answer,str="";
 	int C = number / 100;
 	int remainder = number % 100;
-	/*if (number < 100) answer = numbers_lt_100_to_string(number);*/
-	switch(C){	
-	case 1:answer = "Сто";break;
-	case 2:answer = "Двести";break;
-	case 3:answer = "Триста";break;
-	case 4:answer = "Четыреста";break;
-	case 5:answer = "Пятсот";break;
-	case 6:answer = "Шестьсот";break;
-	case 7:answer = "Семьсот";break;
-	case 8:answer = "Восемьсот";break;
-	case 9:answer = "Девятсот";break;
-	default:break;
-	}
-	if (remainder != 0)
-	{
-	answer +=" "+numbers_lt_100_to_string(remainder);
-	}
+		switch(C){	
+			case 1:answer = "Сто";break;
+			case 2:answer = "Двести";break;
+			case 3:answer = "Триста";break;
+			case 4:answer = "Четыреста";break;
+			case 5:answer = "Пятсот";break;
+			case 6:answer = "Шестьсот";break;
+			case 7:answer = "Семьсот";break;
+			case 8:answer = "Восемьсот";break;
+			case 9:answer = "Девятсот";break;
+			default:break;
+		}
+	if (remainder != 0 && number>100)  
+		str+=" "+ numbers_lt_100_to_string(remainder);
+	if (number<100)
+		answer=numbers_lt_100_to_string(remainder);
+	answer+=str;
 	return answer;
+}
+
+string object_100_to_string(int number, string s1, string s2, string s3){
+	string answer;
+	int remainder = number % 10;
+	answer = numbers_lt_1000_to_string(number);
+	if (number > 10 & number < 15)
+		answer = answer + " " + s3;
+	else
+		switch (remainder)
+	{
+		case 1: answer += " " + s1;break; 
+		case 2: answer += " " + s2;break;
+		case 3: answer += " " + s2;break;
+		case 4: answer += " " + s2;break;
+		default: answer += " " + s3;break;
+	}
+}
+
+string currency_to_string(int integer_value, string si1, string si2, string si3, 
+						  int decimal_value, string sd1, string sd2, string sd3)
+{
+    string answer;
+	answer = object_100_to_string(integer_value, si1, si2, si3);
+	if (decimal_value)
+		answer += ", " + object_100_to_string(integer_value, si1, si2, si3);
+    return answer;
 }
 
 
