6fa85401d0280970a80236ac2602a26496a2ed24
Witawe <texys@list.ru>
2019-04-25 09:25:29 +0900

    lol

--- Game/Game/Source.cpp
+++ Game/Game/Source.cpp
@@ -1,25 +1,16 @@
 #include <iostream>
 #include <windows.h>
 #include <ctime>
+#include <vector>
+
 using namespace std;
 
-int main{
-	return 0;
-}
-int generation(int d1, int d2){
-	POINT kva;
-	HWND hwnd = GetConsoleWindow();
-	HDC hdc = GetDC(hwnd);
-	SelectObject(hdc, GetStockObject(WHITE_PEN));
+struct Point { double x, y ;};
 
-	MoveToEx(hdc,50,50,&kva);
-	LineTo(hdc, 200, 50);
+vector<Point*> generate_points_in_square(int n, double a);
 
-	ReleaseDC (hwnd, hdc);
-	cin.get();
-	
-	srand(time(NULL));
-	int x = d1 + rand() % d2;
-	int y = d1 + rand() % d2;
-	return 0;
+int main(){
+	setlocale(LC_ALL,"RUS");
+	cout<<"אüאי בכ‎"<<endl;
+	system("pause");
 }
